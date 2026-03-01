5656509cacf143259e3dd5fdf7de1a74d2e090dd
srinu-69 <naidusrinivas525@gmail.com>
2026-03-02 00:41:31 +0530

    docker added

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",
+        host="db",  # Use the service name defined in docker-compose.yml
         user="pyuser",
         password="py123",
         database="pypro"
