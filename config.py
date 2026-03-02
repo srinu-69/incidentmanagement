6f924e1d7471ec1937416af9828538597a86b2f0
srinu-69 <naidusrinivas525@gmail.com>
2026-03-02 09:14:41 +0530

    config changed

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),
+        host=os.getenv("DB_HOST", "db"),
         user=os.getenv("DB_USER", "pyuser"),
         password=os.getenv("DB_PASSWORD", "py123"),
         database=os.getenv("DB_NAME", "pypro")
