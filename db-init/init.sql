f82a62ac7044396e81b9f636887faff1d6615eba
srinu-69 <naidusrinivas525@gmail.com>
2026-03-02 23:16:21 +0530

    yml and sql added

---
+++ db-init/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS incidents_p (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    incident_code VARCHAR(50),
+    service VARCHAR(100),
+    severity VARCHAR(50),
+    description TEXT,
+    status VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
