8ed57a99120d63c425678171076db2b25b6386fe
srinu-69 <naidusrinivas525@gmail.com>
2026-03-13 17:21:49 +0530

    init changed

--- db-init/init.sql
+++ db-init/init.sql
@@ -1,11 +1,13 @@
+CREATE DATABASE IF NOT EXISTS pypro;
 
+USE pypro;
 
 CREATE TABLE IF NOT EXISTS incidents_p (
     id INT AUTO_INCREMENT PRIMARY KEY,
     incident_code VARCHAR(50),
     service VARCHAR(100),
     severity VARCHAR(50),
-    description TEXT,s
+    description TEXT,
     status VARCHAR(50),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
